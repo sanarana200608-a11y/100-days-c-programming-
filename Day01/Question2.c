2fc2ec248a269185137f81579c9dfaa1cfec856c
sanarana200608-a11y <sanarana20.06.08@gmail.com>
2026-09-01 22:35:08 +0530

    Implement basic arithmetic operations

---
+++ Day01/Question2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    float num1, num2 ;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    printf("Sum = %.2f\n" , num1 + num2);
+    printf("Difference = %.2f\n", num1 - num2);
+    printf("Product = %.2f\n", num1*num2);
+
+    if (num2 != 0)
+    {
+        printf("Quotient = %.2f\n", num1/num2);
+    }
+    else
+    {
+        printf("Division by zero is not allowed.\n");
+
+    }
+    return 0;
+}
